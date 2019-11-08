280931f01cce5bc7b21d2e56e2fb0c082255c8f1
popovicV <vladimirpopovic@live.com>
2019-11-08 01:14:43 +0100

    Initialized web app

---
+++ src/main/java/isapsw/team55/ClinicalCenter/ClinicalCenterApplication.java
@@ -0,0 +1,13 @@
+package isapsw.team55.ClinicalCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isapsw/team55/ClinicalCenter/ClinicalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package isapsw.team55.ClinicalCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
